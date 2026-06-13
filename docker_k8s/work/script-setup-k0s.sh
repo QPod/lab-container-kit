4afd32e6aa7a037013ce1123694a4a822e369aba
Bibo Hao <haobibo@users.noreply.github.com>
2026-06-14 00:15:33 +0800

    debug k0s install

--- docker_k8s/work/script-setup-k0s.sh
+++ docker_k8s/work/script-setup-k0s.sh
@@ -1,8 +1,8 @@
 # k0s Air-gap Install: https://docs.k0sproject.io/head/airgap-install/
 
 setup_k0s() {
-     VER_K0S=$(curl -sL https://github.com/k0sproject/k0s/releases.atom | grep 'releases/tag/v' | head -1 | grep -Po '\d[\d.]+' ) \
-  && URL_K0S="https://github.com/k0sproject/k0s/releases/download/v${VER_K0S}%2Bk0s.0/k0s-v${VER_K0S}+k0s.0-${ARCH}" \
+     VER_K0S=$(curl -sL https://docs.k0sproject.io/stable.txt) \
+  && URL_K0S="https://github.com/k0sproject/k0s/releases/download/${VER_K0S}/k0s-${VER_K0S}-${ARCH}" \
   && echo "Downloading k0s version ${VER_K0S} from ${URL_K0S}" \
   && mkdir -pv /opt/k0s && curl -L -o /opt/k0s/k0s ${URL_K0S} \
   && chmod +x /opt/k0s/k0s
@@ -10,8 +10,8 @@ setup_k0s() {
 }
 
 setup_k0s_pack() {
-     VER_K0S=$(curl -sL https://github.com/k0sproject/k0s/releases.atom | grep 'releases/tag/v' | head -1 | grep -Po '\d[\d.]+' ) \
-  && URL_K0S_IMG="https://github.com/k0sproject/k0s/releases/download/v${VER_K0S}+k0s.0/k0s-airgap-bundle-v${VER_K0S}+k0s.0-${ARCH}" \
+     VER_K0S=$(curl -sL https://docs.k0sproject.io/stable.txt) \
+  && URL_K0S_IMG="https://github.com/k0sproject/k0s/releases/download/${VER_K0S}/k0s-airgap-bundle-${VER_K0S}-${ARCH}" \
   && echo "Downloading k0s airgap images version ${VER_K0S} from ${URL_K0S_IMG}" \
-  && curl -L -o "/opt/k0s/k0s-airgap-bundle-v${VER_K0S}+k0s.0-${ARCH}" ${URL_K0S_IMG}
+  && curl -L -o "/opt/k0s/k0s-airgap-bundle-${VER_K0S}-${ARCH}" ${URL_K0S_IMG}
 }
