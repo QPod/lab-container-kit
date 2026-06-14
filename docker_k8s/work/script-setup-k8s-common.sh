1465d9afe4eb3b666d2f550c78cc43044437b869
Bibo Hao <haobibo@users.noreply.github.com>
2026-06-14 20:21:47 +0800

    update folder

--- docker_k8s/work/script-setup-k8s-common.sh
+++ docker_k8s/work/script-setup-k8s-common.sh
@@ -31,3 +31,18 @@ setup_helm() {
   && chmod +x /opt/k8s/helm
   /opt/k8s/helm version
 }
+
+setup_k9s() {
+  # ref: https://github.com/derailed/k9s , the binary is roughly 120MB
+  local K9S_ARCH=$ARCH
+  [ "$K9S_ARCH" = "arm" ] && K9S_ARCH="armv7"
+
+  VER_K9S=$(curl -sL https://github.com/derailed/k9s/releases.atom | grep 'releases/tag/v' | grep -v 'rc' | head -1 | sed -E 's/.*\/tag\/([^"]+).*/\1/') \
+  && URL_K9S="https://github.com/derailed/k9s/releases/download/${VER_K9S}/k9s_Linux_${K9S_ARCH}.tar.gz" \
+  && echo "Downloading K9s version ${VER_K9S} from: ${URL_K9S}" \
+  && curl -L -s -o /tmp/k9s.tar.gz "${URL_K9S}" \
+  && mkdir -pv /opt/k8s \
+  && tar -zxvf /tmp/k9s.tar.gz -C /opt/k8s/ k9s \
+  && chmod +x /opt/k8s/k9s
+  /opt/k8s/k9s version
+}
